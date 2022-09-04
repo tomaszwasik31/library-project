35680dc49a91f9ab3c2380a03a778bcefbee94dc
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-09-04 22:15:13 +0100

    remove btn, work on toogle btn

--- script.js
+++ script.js
@@ -5,31 +5,67 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+const theHobbit = new Book("The Hobbit", "Tolkien", 295, false);
+const witcher = new Book("The Witcher", "Sapkowski", 321, true);
+let myLibrary = [theHobbit, witcher, witcher,  witcher];
+
 function createBookDiv(book) {
   const main = document.getElementById("main");
   const div = document.createElement("div");
   div.id = "book";
   div.innerHTML = `  <p id="book-title">${book.title}</p>
 <p id="book-author">${book.author}</p>
-<p id="book-pages">Number of pages: ${book.pages}</p>
-<button id="book-read">Read</button>
-<button id="book-remove">Remove</button>`;
+<p id="book-pages">Number of pages: ${book.pages}</p>`;
+
+  if (book.read == true) {
+    div.innerHTML += `<p id="read?">Book read?<button class="read-yes">Yes</button></p>`;
+  } else {
+    div.innerHTML += `<p id="read?">Book read?<button class="read-no">No</button></p>`;
+  }
+
+  div.innerHTML += `<button id="book-remove" onclick="return this.parentNode.remove()">Remove</button>`;
   main.appendChild(div);
 }
 
-const theHobbit = new Book("The Hobbit", "Tolkien", 295, false);
-const witcher = new Book("The Witcher", "Sapkowski", 321, true);
-let myLibrary = [theHobbit, witcher];
+function createAddBtn() {
+  const main = document.getElementById("main");
+  const div = document.createElement("div");
+  div.id = "add-btn-wrapper";
+  div.innerHTML = `<button  class="btn" onclick="openForm()">Add book</button>`;
+  main.appendChild(div);
+}
 
 function printBookArray() {
   myLibrary.forEach(function (book) {
     createBookDiv(book);
   });
-  emptyArray();
 }
 
-function emptyArray() {
-  myLibrary = [];
-}
 printBookArray();
-console.log(myLibrary);
+createAddBtn();
+
+//form hide-show
+
+function openForm() {
+  document.getElementById("myForm").style.display = "flex";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+//
+
+let readYes = document.querySelectorAll('.read-yes');
+
+
+
+readYes.forEach(e=>{
+  e.addEventListener("click", yesToNo)
+});
+
+function yesToNo(){
+  readYes.forEach(e=>{
+    e.classList.replace('read-yes', 'read-no')
+  });
+}
